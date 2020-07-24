bfe4bf701cbd013cd26fa9a8c1297ea80c2bd276
A-hungry-wolf <yutao.chen@aliyun.com>
2020-07-24 16:41:58 +0800

    Jul 24 Learning experience

---
+++ Test/test.js
@@ -0,0 +1,21 @@
+/* var mood = "happy";
+var age = 33;
+
+alert(mood);
+alert(age);  */
+
+/* var hight = "about 5'10\" tall";
+alert(hight); */
+
+/* var married = true;
+alert(married); */
+
+/* var beatles = Array(4);
+beatles[0] = "John";
+beatles[1] = "Paul";
+beatles[2] = "George";
+beatles[3] = "Ringo";
+alert(beatles); */
+
+/* var beatles = Array("John","paul","George","Ringo");
+alert(beatles); */
