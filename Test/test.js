8b37973d9223e2f158e80e8602d3e5f9b9d0c7b9
A-hungry-wolf <yutao.chen@aliyun.com>
2020-07-29 10:27:55 +0800

    today's learning update

--- Test/test.js
+++ Test/test.js
@@ -40,12 +40,106 @@ if(my_mood != your_mood){
 } */
 
 
-var a = false;
+/* var a = false;
 var b = "";
 if(a === b){
     alert("a equals to b");
 }else{
     alert("a != b");
+} */
+
+
+
+/* var num = 2 * 3 ;
+if( num >= 5 && num <= 10 ){
+    alert("The number is in the right range.");
+} */
+
+
+/* var num = 2 * 3;
+alert(num);
+if(!(num < 5 || num > 10)){
+    alert("the number is not in the righr zoom");
 }
+alert("run successfully"); */
+
+
+/* var count = 1;
+while(count < 11){
+    alert(count);
+    count++;
+} */
+
+
+/* var count = 1;
+do{
+    alert(count);
+    count++
+}while(count < 1);  */
+
+
+/* for(var count = 1; count < 11; count ++){
+    alert(count);
+} */
+
+
+/* var beatles = Array("John","Paul","George","Ringo");
+for(var count = 0; count < beatles.length; count++){
+    alert(beatles[count]);
+} */
+
+
+/* function shout(){
+    var beatles = Array("John","Paul","George","Ringo")
+    for(var count = 0; count < beatles.length; count++){
+        alert(beatles[count]);
+    }
+}
+shout(); */
+
+
+/* function multiply(num1,num2){
+    var total = num1 * num2;
+    alert(total);
+}
+multiply(10,2); */
+
+
+/* alert("run successfully");
+
+
+var temp_fahrenheit = 95;
+var temp_celsius = convertToCelsius(temp_fahrenheit);
+alert(temp_celsius);
+
+
+function convertToCelsius(temp){
+    var result = temp - 32;
+    result = result / 1.8;
+    return result;
+} */
+
+
+/* function square(num){
+    total = num * num;
+    return total;
+}
+var total = 50;
+var number = square(20);
+alert(total); */
+
+
+/* function square(num){
+    var total = num * num;
+    return total;
+}
+var total = 50;
+var number = square(20);
+alert(total); */
+
+
+
+
+
 
 
