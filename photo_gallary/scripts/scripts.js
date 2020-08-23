5b59d8c890a4d850bb91aaaf8636674907281b93
A-hungry-wolf <yutao.chen@aliyun.com>
2020-08-23 18:08:29 +0800

    添加insertAter函数,实现html与JavaScript分离

--- photo_gallary/scripts/scripts.js
+++ photo_gallary/scripts/scripts.js
@@ -46,3 +46,33 @@ function countBodyChildren(){
 }
 window.onload = countBodyChildren;
 */
+
+
+
+
+function insertAfter(newElement,targetElement){
+	var parent = targetElement.parentNode;
+	if (parent.lastChild == targetElement) {
+		parent.appendChild(newElement);
+	}else{
+		parent.insertBefore(newElement,targetElement.nextSibling);
+	}
+}
+
+
+function preparePlaceholder(){
+	var placeholder = document.createElement("img");
+	placeholder.setAttribute("id","placeholder");
+	placeholder.setAttribute("src","images/placeholder.jpg");
+	placeholder.setAttribute("alt","my image gallery");
+	var description = document.createElement("p");
+	description.setAttribute("id","description");
+	var desctext = document.createTextNode("Choose an image");
+	description.appendChild(desctext);
+	var gallery = document.getElementById("imagegallery");
+	insertAfter(placeholder,gallery);
+	insertAfter(description,placeholder);
+}
+
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
